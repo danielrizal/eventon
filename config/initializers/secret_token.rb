596378ba784b68fcfbc56098f70364c2ff9a0d06
Daniel Rizal <flipinteract@gmail.com>
2013-10-30 00:23:12 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventon::Application.config.secret_key_base = '46df58ac1e655f99482a6be08981b76167df01ca0be039af20f13a0ce23122adf634d87af0ecc5fdd463c7baf4023e95ba3699f15a7c8f11a8c2d34dc78b3a9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eventon::Application.config.session_store :cookie_store, key: '_eventon_session'
