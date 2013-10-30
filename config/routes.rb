5d622ff6b52c3c3e6c8ddd48f6a979a929197be2
Daniel Rizal <flipinteract@gmail.com>
2013-10-30 17:48:08 +1100

    db migrated, usr created, team page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Eventon::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" # Creates about path
   get "services" => "pages#services" # Creates contact path
